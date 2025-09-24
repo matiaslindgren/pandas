1be15dbccfcf1aa897a43016634ef1cb0c7db539
Matias Lindgren <matias.lindgren@iki.fi>
2025-09-24 12:31:50 -0400

    create env if missing

--- env.bash
+++ env.bash
@@ -1,2 +1,5 @@
 eval "$($HOME/miniforge3/bin/mamba shell hook --shell bash)"
+if [ ! -d $HOME/miniforge3/envs/pandas-dev ]; then
+	mamba env create --file environment.yml
+fi
 mamba activate pandas-dev
